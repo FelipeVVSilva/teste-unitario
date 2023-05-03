00a05791045e3a14a3901bd634419a71ca7a5dbd
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-05-03 16:15:46 -0300

    Teste PlanetService com cenário de erro

--- sw-planet-api/sw-planet-api/src/test/java/com/example/swplanetapi/common/PlanetConstants.java
+++ sw-planet-api/sw-planet-api/src/test/java/com/example/swplanetapi/common/PlanetConstants.java
@@ -5,5 +5,6 @@ import com.example.swplanetapi.domains.Planet;
 public class PlanetConstants {
     
     public static final Planet PLANET = new Planet(null, "name", "climate", "terrain");
+    public static final Planet INVALID_PLANET = new Planet(null, "", "","");
 
 }
--- sw-planet-api/sw-planet-api/src/test/java/com/example/swplanetapi/services/PlanetServiceTest.java
+++ sw-planet-api/sw-planet-api/src/test/java/com/example/swplanetapi/services/PlanetServiceTest.java
@@ -1,20 +1,17 @@
 package com.example.swplanetapi.services;
 
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static com.example.swplanetapi.common.PlanetConstants.INVALID_PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.example.swplanetapi.common.PlanetConstants;
 import com.example.swplanetapi.domains.Planet;
 import com.example.swplanetapi.repositories.PlanetRepository;
 
@@ -36,5 +33,14 @@ public class PlanetServiceTest {
 
         assertThat(sut).isEqualTo(PLANET);
     }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException(){
+
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+        assertThatThrownBy(() -> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+    }
+
 
 }
