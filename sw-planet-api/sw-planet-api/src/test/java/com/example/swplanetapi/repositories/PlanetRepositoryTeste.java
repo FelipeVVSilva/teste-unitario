19844e1142feeef1aa85b831c503de54e39a631f
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-05-04 15:22:29 -0300

    Teste de integração createPlanet com dados inválidos - PlanetRepositoryTeste

--- sw-planet-api/sw-planet-api/src/test/java/com/example/swplanetapi/repositories/PlanetRepositoryTeste.java
+++ sw-planet-api/sw-planet-api/src/test/java/com/example/swplanetapi/repositories/PlanetRepositoryTeste.java
@@ -1,6 +1,7 @@
 package com.example.swplanetapi.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import com.example.swplanetapi.domains.Planet;
 
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static com.example.swplanetapi.common.PlanetConstants.INVALID_PLANET;;
 
 @DataJpaTest
 public class PlanetRepositoryTeste {
@@ -30,6 +32,27 @@ public class PlanetRepositoryTeste {
         assertThat(sut.getName()).isEqualTo(planet.getName());
         assertThat(sut.getClimate()).isEqualTo(planet.getClimate());
         assertThat(sut.getTerrain()).isEqualTo(planet.getTerrain());
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException(){
+
+        Planet emptyPlanet = new Planet();
+        Planet invalidPlanet = new Planet(null, "", "", "");
+
+        assertThatThrownBy(() -> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);;
+    }
+
+    @Test
+    public void createPlanet_WithExistingName_ThrowwsException(){
+
+        Planet planet = testEntityManager.persistFlushFind(PLANET);
+        testEntityManager.detach(planet);
+        planet.setId(null);
+
+        assertThatThrownBy(() -> planetRepository.save(planet)).isInstanceOf(RuntimeException.class);
+
     }
 
 }
