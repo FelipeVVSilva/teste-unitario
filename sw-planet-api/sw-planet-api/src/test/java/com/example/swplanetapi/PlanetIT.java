e7b21629ff576d0fe5f72ddf6e7bdb7b012bdcb0
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-05-08 17:08:53 -0300

    Testando cadastro com sucesso

--- sw-planet-api/sw-planet-api/src/test/java/com/example/swplanetapi/PlanetIT.java
+++ sw-planet-api/sw-planet-api/src/test/java/com/example/swplanetapi/PlanetIT.java
@@ -1,16 +1,40 @@
 package com.example.swplanetapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.swplanetapi.domains.Planet;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"/remove_planets.sql"}, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
 public class PlanetIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
     
     @Test
-    public void contextLoads(){
+    public void createPlanet_ReturnsCreated(){
+
+        Planet sut = webTestClient.post().uri("/planets").bodyValue(PLANET)
+            .exchange().expectStatus().isCreated().expectBody(Planet.class)
+            .returnResult().getResponseBody();
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getId()).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+
     }
 
 
