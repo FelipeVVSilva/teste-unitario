409f368ed6e37ad2b55fb09d9d9e41845545db8c
FelipeVeigaVellosoDaSilva <felipe.veiga.trabalho@gmail.com>
2023-05-09 09:12:03 -0300

    Separando as fazes de teste

--- sw-planet-api/sw-planet-api/src/main/java/com/example/swplanetapi/repositories/PlanetRepository.java
+++ sw-planet-api/sw-planet-api/src/main/java/com/example/swplanetapi/repositories/PlanetRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.example.swplanetapi.domains.Planet;
